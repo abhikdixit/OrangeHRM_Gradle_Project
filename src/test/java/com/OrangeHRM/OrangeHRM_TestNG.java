71b94cc41d45f73a670b0760847cb85fdce56d77
abhikdixit <abhinay.dixit@hotmail.com>
2021-02-04 16:13:27 +0530

    Adding Gradle Project

---
+++ src/test/java/com/OrangeHRM/OrangeHRM_TestNG.java
@@ -0,0 +1,24 @@
+package com.OrangeHRM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OrangeHRM_TestNG {
+	@Test
+	public void Login() {
+		WebDriverManager.firefoxdriver().setup();
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		// driver.navigate().to("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.findElementByName("txtUsername").sendKeys("Admin");
+		driver.findElementByName("txtPassword").sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		// Checkpoint ( Verifying the Exp vs Act result)
+		driver.findElementByLinkText("Dashboard").isDisplayed();
+		// driver.close();//Close will close current browser opened by WebDriver
+		driver.quit(); // Quit will close all browser opened by WebDriver
+	}
+}
